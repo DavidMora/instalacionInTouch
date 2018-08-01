edfe613713b23983203cec1f71782bc2a2e84cc1
Camilo Pérez <camilo.perezvr@gmail.com>
2018-08-01 11:47:02 -0500

    Agregar variable para carpeta de in updater

--- create-release.sh
+++ create-release.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tar -czf install.tar.gz boot etc home inupdater certificates splash usr auto-install.sh auto-install-inmote.sh README.md
+tar -czf install.tar.gz boot etc home inupdater certificates splash usr auto-install.sh auto-install-inmote.sh auto-install-update.sh README.md
